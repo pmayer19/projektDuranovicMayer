584784c9c2943fb7e1921c871309b3f99fb4eb44
Enis Duranovic <DuranovicE190050@sus.htl-grieskirchen.at>
2022-01-17 19:48:59 +0100

    commit new

---
+++ Projekt_Mayer_Duranovic/src/net/htlgkr/duranovice190050/Main.java
@@ -0,0 +1,10 @@
+package net.htlgkr.duranovice190050;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("♕");
+        System.out.println("♛ b");
+        System.out.println("♘");
+    }
+}
